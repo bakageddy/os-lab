b5cb2ad9ef2f5b5dc4d54c7547f352fb767590f3
Dinesh Kumar G <gdineshda@gmail.com>
2023-01-23 19:42:59 +0530

    Experiment 2E

--- Experiment2/experiment2e.c
+++ Experiment2/experiment2e.c
@@ -2,10 +2,23 @@
 #include <stdlib.h>
 #include <dirent.h>
 
-#include <sys/dir.h>
-#include <time.h>
+int main(int argc, char** argv) {
+    struct dirent *dir;
+    DIR *dname;
 
+    if (argc != 2) {
+        puts("Usage: ./a.out <dirname>");
+        exit(-1);
+    }
 
-int main(void) {
+    dname = opendir(argv[1]);
+    if (dname == NULL) {
+        perror(argv[1]);
+        exit(-1);
+    }
+
+    while ((dir = readdir(dname)) != NULL)
+        puts(dir -> d_name);
+
     return 0;
 }
